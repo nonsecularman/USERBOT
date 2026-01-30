419485dc6f8aa9c9e790a129fcac71356cdd7e3f
nonsecularman <krishbharti3404@gmail.com>
2026-01-31 03:20:42 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,27 +1,25 @@
 import os
 
-# ---- REQUIRED ----
-API_ID = 34777835
-API_HASH = "5f3ef62d9462a01211bde888e770f715"
+API_ID = int(os.environ.get("API_ID"))
+API_HASH = os.environ.get("API_HASH")
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
 
-BOT_TOKEN = "8540101865:AAHvFfqUri2XeMs9G2ndDTI-XMo62EIhR0o"
+OWNER_ID = int(os.environ.get("OWNER_ID"))
 
-OWNER_ID = 7847600903
+ALIVE_PIC = os.environ.get(
+    "ALIVE_PIC",
+    "https://telegra.ph/file/e973fdcba1e5d102e64b9.jpg"
+)
 
-# ---- ALIVE PIC ----
-ALIVE_PIC = "https://telegra.ph/file/e973fdcba1e5d102e64b9.jpg"   # koi bhi image link daal sakta hai
+SUDO_USERS = [OWNER_ID]
 
-# ---- OPTIONAL ----
-STRING_SESSION1 = ""
-STRING_SESSION2 = ""
-STRING_SESSION3 = ""
-STRING_SESSION4 = ""
-STRING_SESSION5 = ""
-STRING_SESSION6 = ""
-STRING_SESSION7 = ""
-STRING_SESSION8 = ""
-STRING_SESSION9 = ""
-STRING_SESSION10 = ""
-
-# ---- SUDO USERS ----
-SUDO_USERS = [OWNER_ID]   # owner already included
+STRING_SESSION1 = os.environ.get("STRING_SESSION1", "")
+STRING_SESSION2 = os.environ.get("STRING_SESSION2", "")
+STRING_SESSION3 = os.environ.get("STRING_SESSION3", "")
+STRING_SESSION4 = os.environ.get("STRING_SESSION4", "")
+STRING_SESSION5 = os.environ.get("STRING_SESSION5", "")
+STRING_SESSION6 = os.environ.get("STRING_SESSION6", "")
+STRING_SESSION7 = os.environ.get("STRING_SESSION7", "")
+STRING_SESSION8 = os.environ.get("STRING_SESSION8", "")
+STRING_SESSION9 = os.environ.get("STRING_SESSION9", "")
+STRING_SESSION10 = os.environ.get("STRING_SESSION10", "")
