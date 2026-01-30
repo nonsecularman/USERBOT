f098b008f74f1ed293beec0f8c8a86a793aad671
nonsecularman <krishbharti3404@gmail.com>
2026-01-31 03:44:53 +0530

    Update __init__.py

--- Zaid/modules/__init__.py
+++ Zaid/modules/__init__.py
@@ -1,21 +1,9 @@
-import glob
-from os.path import dirname, isfile
+import os
 
+ALL_MODULES = []
 
-def __list_all_modules():
-    work_dir = dirname(__file__)
-    mod_paths = glob.glob(work_dir + "/*/*.py")
+for file in os.listdir(os.path.dirname(__file__)):
+    if file.endswith(".py") and not file.startswith("_"):
+        ALL_MODULES.append(file[:-3])
 
-    all_modules = [
-        (((f.replace(work_dir, "")).replace("/", "."))[:-3])
-        for f in mod_paths
-        if isfile(f)
-        and f.endswith(".py")
-        and not f.endswith("__init__.py")
-    ]
-
-    return all_modules
-
-
-ALL_MODULES = sorted(__list_all_modules())
 __all__ = ALL_MODULES + ["ALL_MODULES"]
