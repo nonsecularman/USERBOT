104e3795d36edab01629f77c67154c3c08d7275c
nonsecularman <krishbharti3404@gmail.com>
2026-01-31 12:36:30 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,25 +1,44 @@
 import os
+from os import getenv
+from dotenv import load_dotenv
 
-API_ID = int(os.environ.get("API_ID"))
-API_HASH = os.environ.get("API_HASH")
-BOT_TOKEN = os.environ.get("BOT_TOKEN")
+if os.path.exists("local.env"):
+    load_dotenv("local.env")
 
-OWNER_ID = int(os.environ.get("OWNER_ID"))
 
-ALIVE_PIC = os.environ.get(
+API_ID = int(getenv("API_ID", "6435225"))  # optional
+API_HASH = getenv("API_HASH", "4e984ea35f854762dcde906dce426c2d")  # optional
+
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "").split()))
+
+# ✅ FIX: OWNER_ID safe default
+OWNER_ID = int(getenv("OWNER_ID", "0"))
+
+# ✅ FIX: MONGO_URL safe default
+MONGO_URL = getenv("MONGO_URL", "")
+
+BOT_TOKEN = getenv("BOT_TOKEN", "")
+
+ALIVE_PIC = getenv(
     "ALIVE_PIC",
-    "https://telegra.ph/file/e973fdcba1e5d102e64b9.jpg"
+    "https://telegra.ph/file/3c52a01057865f7511168.jpg"
 )
 
-SUDO_USERS = [OWNER_ID]
+ALIVE_TEXT = getenv("ALIVE_TEXT", "Bot is Alive 🤖")
+PM_LOGGER = getenv("PM_LOGGER", "")
+LOG_GROUP = getenv("LOG_GROUP", "")
 
-STRING_SESSION1 = os.environ.get("STRING_SESSION1", "")
-STRING_SESSION2 = os.environ.get("STRING_SESSION2", "")
-STRING_SESSION3 = os.environ.get("STRING_SESSION3", "")
-STRING_SESSION4 = os.environ.get("STRING_SESSION4", "")
-STRING_SESSION5 = os.environ.get("STRING_SESSION5", "")
-STRING_SESSION6 = os.environ.get("STRING_SESSION6", "")
-STRING_SESSION7 = os.environ.get("STRING_SESSION7", "")
-STRING_SESSION8 = os.environ.get("STRING_SESSION8", "")
-STRING_SESSION9 = os.environ.get("STRING_SESSION9", "")
-STRING_SESSION10 = os.environ.get("STRING_SESSION10", "")
+GIT_TOKEN = getenv("GIT_TOKEN", "")  # personal access token
+REPO_URL = getenv("REPO_URL", "https://github.com/ITZ-ZAID/ZAID-USERBOT")
+BRANCH = getenv("BRANCH", "master")  # don't change
+
+STRING_SESSION1 = getenv("STRING_SESSION1", "")
+STRING_SESSION2 = getenv("STRING_SESSION2", "")
+STRING_SESSION3 = getenv("STRING_SESSION3", "")
+STRING_SESSION4 = getenv("STRING_SESSION4", "")
+STRING_SESSION5 = getenv("STRING_SESSION5", "")
+STRING_SESSION6 = getenv("STRING_SESSION6", "")
+STRING_SESSION7 = getenv("STRING_SESSION7", "")
+STRING_SESSION8 = getenv("STRING_SESSION8", "")
+STRING_SESSION9 = getenv("STRING_SESSION9", "")
+STRING_SESSION10 = getenv("STRING_SESSION10", "")
