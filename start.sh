32421692aad86aae50d09c0c69e86ed1631aad09
nonsecularman <krishbharti3404@gmail.com>
2026-01-31 16:58:03 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,12 @@
-python main.py
+#!/usr/bin/env bash
+python - << 'EOF'
+import asyncio
+from main import bot  # the bot instance
+
+async def main():
+    await bot.start()
+    print("✅ BOT STARTED")
+    await bot.run_until_disconnected()
+
+asyncio.run(main())
+EOF
